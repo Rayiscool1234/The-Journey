9edc33c3f6051069059c7a01874999f58b9e3429
rayan <rayan@DESKTOP-H5P4B9M>
2022-08-27 17:12:48 +0300

    Add project files.

---
+++ The Journey/The Journey.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+
+
+
+
+
+int Menu() {
+
+	std::cout << "Welcome to The Journey of YOU\n1-Start Game\n2-Options\n3-Credits\n4-Exit Game" << std::endl;
+
+
+	
+	return(0);
+
+
+}
+
+
+int Selection(int Num) {
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+int main() {
+
+
+	Menu();
+
+
+	
+}
